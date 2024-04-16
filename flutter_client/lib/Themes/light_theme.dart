f71e661ab8694e24ffa3a88473949035c8aa8364
Hirbod Khatami <hirbod.khatami@gmail.com>
2024-04-16 15:10:45 +0000

    feat: Update light_theme.dart

--- flutter_client/lib/Themes/light_theme.dart
+++ flutter_client/lib/Themes/light_theme.dart
@@ -5,7 +5,7 @@ ThemeData lightTheme = FlexThemeData.light(
   appBarOpacity: 0,
   appBarElevation: 0,
   colors: const FlexSchemeColor(
-    primary: Color(0xFF08740A),
+    primary: Color(0xFF08744B),
     primaryContainer: Color(0xff9ee29f),
     secondary: Color(0xff365b37),
     secondaryContainer: Color(0xFF8FBF92),
